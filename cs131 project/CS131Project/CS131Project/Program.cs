638a372f0a99f456932fdc6546bdba93ef3cb452
Mario Silva <79116487+msilva5900@users.noreply.github.com>
2021-02-18 15:35:52 -0700

    Add files via upload
    I have made the corrections for the assignment. Please let me know if correct.

---
+++ cs131 project/CS131Project/CS131Project/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CS131Project
+{
+    class Program
+
+   
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine();
+            Console.WriteLine("Module 1 Assignment");
+
+            int myVariable1 = 1;
+            int myVariable2 = 3;
+            int myVariable3 = 7;
+
+
+            Console.WriteLine(myVariable1);
+            Console.WriteLine(myVariable2);
+
+            string myVariablestring = ("This is my second attempt");
+            string myVariablestring2 = ("I am hoping it is correct this time");
+            Console.WriteLine(myVariable1 * myVariable2);
+
+            Console.WriteLine(myVariablestring);
+
+
+            Console.WriteLine(myVariable1 / myVariable2);
+            Console.WriteLine(myVariablestring2);
+
+            Console.WriteLine(myVariable1 + myVariable3);
+
+        }
+    }
+}
+
+
+
